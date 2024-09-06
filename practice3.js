dd64ca98009a37f07c47377cbfe777c54992104b
blockcati <aniqueali11@gmail.com>
2024-09-06 21:18:06 +0500

    Traffic System
    Using if statement

---
+++ practice3.js
@@ -0,0 +1,13 @@
+let light=red;
+
+if(light === "red"){
+    console.log("Stop" + " " + "Traffic is close")
+    }
+    if(light === "Green"){
+        console.log("GO" + " " + "Traffic is Open")
+        }
+        if(light === "Yellow"){
+            console.log("Slow" + " " + "Traffic is closing")
+            }
+        
+    
