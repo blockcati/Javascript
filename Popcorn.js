21d92954bcacac0fe7e7e62487c6b7c108f2bb1e
blockcati <aniqueali11@gmail.com>
2024-09-06 21:59:46 +0500

    Popcorn Selling System
    Using IF Else Statements

---
+++ Popcorn.js
@@ -0,0 +1,14 @@
+let ask = 100;
+
+if(ask==250){
+    console.log("This XL popcorn");
+} 
+else if(ask ==200){
+    console.log("This is Large" + " " + "Popcorn");
+}
+else if(ask == 100){
+    console.log("This is Medium" + " " + "Popcorn");
+}
+else if (ask == 50){
+    console.log("This is Small" + " " + "Popcorn");
+}
