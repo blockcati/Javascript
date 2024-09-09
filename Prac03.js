fe942b803a8b62981740c5ad873017a6eeedc94d
blockcati <aniqueali11@gmail.com>
2024-09-09 23:53:06 +0500

    strmethods

---
+++ Prac03.js
@@ -0,0 +1,8 @@
+let greeting = "Good Morning";
+
+console.log(greeting.length);
+console.log(greeting.toUpperCase());
+console.log(greeting.toLowerCase());
+console.log(greeting.slice(0, 4));
+console.log(greeting.replace("Morning", "Evening"));
+console.log(greeting.includes("Good"));
