3958e3ef1757b53673c0679269afdcdcae1db36e
blockcati <aniqueali11@gmail.com>
2024-09-09 23:52:25 +0500

    If-else02

---
+++ Prac02.js
@@ -0,0 +1,11 @@
+let score = 75;
+
+if (score >= 90) {
+  console.log("Grade: A");
+} else if (score >= 80) {
+  console.log("Grade: B");
+} else if (score >= 70) {
+  console.log("Grade: C");
+} else {
+  console.log("Grade: F");
+}
