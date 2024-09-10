593873f532a259051f8dc02817d2821530cdc2f0
blockcati <aniqueali11@gmail.com>
2024-09-11 01:06:48 +0500

    Foor Loop Example Qs

---
+++ forloop.js
@@ -0,0 +1,16 @@
+for(let i = 0; i<=10; i++){
+    console.log(i);
+}
+
+let A = Anique;
+
+if(a.strIndex>=2){
+    console.log("Hello");
+    
+}else{
+    console.log("World");
+}
+
+for(let Anique=2; Anique.strIndex<=10; Anique++){
+    console.log(Anique)
+}
