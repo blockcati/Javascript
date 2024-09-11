b9cca95699d8b9a923bdf8ee4bbffa82bc503ac6
blockcati <aniqueali11@gmail.com>
2024-09-12 00:52:40 +0500

    Update Values

---
+++ Add_updateValues.js
@@ -0,0 +1,13 @@
+const Student = {
+    name: "Anique",
+    age: 20,
+    marks: 94.4,
+    city: "Islamabad",
+}
+Student.city="Karachi";
+Student.Gender="Make";
+
+Student.marks="A";
+Student.marks=[90,30,40]
+delete Student.age;
+console.log(Student);
