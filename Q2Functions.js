6f2b601e489ed55248a2483e1e5cd6efc8b63513
blockcati <aniqueali11@gmail.com>
2024-09-15 02:29:33 +0500

    2

---
+++ Q2Functions.js
@@ -0,0 +1,14 @@
+country = ["Australia","Germany","United States of America"];
+
+function Longestcountry(country){
+    let ansIdx=0;
+
+    for(i=0; i<country.length; i++){
+    
+
+
+    }
+
+
+
+}
