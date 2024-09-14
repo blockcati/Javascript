9ffa48cdda31ee501dcfc9dbdcc6d07721336258
blockcati <aniqueali11@gmail.com>
2024-09-15 01:25:19 +0500

    Multi_para

---
+++ Multipleparameters.js
@@ -0,0 +1,8 @@
+function CalculateTotalcost(priceperItem,quantity){
+    let totalcost = priceperItem * quantity;
+    return totalcost;
+
+}
+
+console.log(CalculateTotalcost(5, 3)); 
+console.log(CalculateTotalcost(10, 2));
