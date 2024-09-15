78290e8308f1cd3568c5c0f409854bece44a925e
blockcati <aniqueali11@gmail.com>
2024-09-16 00:09:44 +0500

    For each Function

---
+++ foreach.js
@@ -0,0 +1,19 @@
+let arr= [
+{
+   Name: "Anique",
+   Marks: 90,
+
+},
+{
+Name: "Uzair",
+Marks: 85,
+},
+{
+Name:"Saad",
+Marks: 95,
+}
+]
+
+arr.forEach((Student)=>{
+console.log(Student.Marks)
+})
