85ae5d3e5b89b2e6ca220e1a60dc253d26920140
blockcati <aniqueali11@gmail.com>
2024-09-17 04:32:27 +0500

    DOM Practice JS

---
+++ DOM.js
@@ -0,0 +1,13 @@
+//Selecting Elements
+var a =  document.querySelector("h1");
+
+console.log(a);
+
+//Chaning the Html
+
+a.innerHTML="Changed HTML";
+
+//CSS
+
+a.style.color="red";
+a.style.backgroundColor="Black";
