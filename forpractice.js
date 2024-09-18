291e4471ab72efa2e995d2158994506c6645971d
blockcati <aniqueali11@gmail.com>
2024-09-18 23:25:07 +0500

    practice

---
+++ forpractice.js
@@ -0,0 +1,20 @@
+let arr= [
+    {
+       Name: "Khasn",
+       Marks: 90,
+    
+    },
+    {
+    Name: "Uzair",
+    Marks: 85,
+    },
+    {
+    Name:"Saad",
+    Marks: 95,
+    }
+    ]
+    
+    arr.forEach((Student)=>{
+    console.log(Student.Marks)
+    })
+    
